919fcecc8ce597ae54399d1fa017d8c3f6082596
Jan Jeřábek <xjerab13@stud.fit.vutbr.cz>
2013-10-20 23:48:10 +0200

    Initial class touch.

---
+++ src/cz/vutbr/fit/pdb/containers/SpatialContainer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.vutbr.fit.pdb.containers;
+
+/**
+ *
+ * @author casey
+ */
+public class SpatialContainer {
+    
+}
---
+++ src/cz/vutbr/fit/pdb/view/MapPanel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.vutbr.fit.pdb.view;
+
+/**
+ *
+ * @author casey
+ */
+public class MapPanel {
+    
+}
