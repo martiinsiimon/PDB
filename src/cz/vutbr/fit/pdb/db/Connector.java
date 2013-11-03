969e61366ff1cb6a0cff446ae980db0eec5ccaca
Martin Simon <martiin.siimon@gmail.com>
2013-11-03 23:08:06 +0100

    Database execution class

--- src/cz/vutbr/fit/pdb/db/Connector.java
+++ src/cz/vutbr/fit/pdb/db/Connector.java
@@ -1,11 +1,15 @@
 package cz.vutbr.fit.pdb.db;
 
 import java.sql.Connection;
-//import java.sql.ResultSet;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.SQLException;
-//import java.sql.Statement;
+
+import java.util.Stack;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import oracle.jdbc.pool.OracleDataSource;
 
 /**
@@ -17,41 +21,105 @@ public class Connector {
     private static String url = "jdbc:oracle:thin:@berta.fit.vutbr.cz:1522:dbfit";
     private String login;
     private String password;
-    //TODO define internal stack to handle all queries in queue
+    private Stack<String> queries;
 
     public Connector(String login, String password) {
         this.login = login;
         this.password = password;
-        //TODO initialize the internal stack to be free
+        queries.clear();
     }
 
-    public void addCommit(String query) {
-        /* Add a commit to internal stack */
-        //TODO add query into stack
-        System.out.print(query);
+    /**
+     * Add a query to the internal stack
+     *
+     * @param query Query to be added to the queue
+     */
+    public void addQuery(String query) {
+        this.queries.push(query);
     }
 
+    /**
+     * Delete the last commit from queue
+     */
+    public void delCommit() {
+        this.queries.pop();
+    }
+
+    /**
+     * Returns connection to DB
+     *
+     * @return New connection to database
+     * @throws SQLException
+     */
+    private Connection getConnection() throws SQLException {
+        /* Create a OracleDataSource instance */
+        OracleDataSource ods = new OracleDataSource();
+        ods.setURL(Connector.url);
+        ods.setUser(this.login);
+        ods.setPassword(this.password);
+
+        /* Connect to the database */
+        return ods.getConnection();
+    }
+
+    /**
+     * Execute update (insert,update,delete) queries stored in internal stack
+     */
     public void executeQuery() {
-        /* Execute all commits in internal stack in one connection */
-
         try {
-            // create a OracleDataSource instance
-            OracleDataSource ods = new OracleDataSource();
-            ods.setURL(Connector.url);
-            ods.setUser(this.login); // command line: ... -Dlogin=xnovak99 ...
-            ods.setPassword(this.password); // command line: ... -Dpassword=tajneheslo ...
-            // connect to the database
-            Connection conn = ods.getConnection();
+            /* Connect to the database */
+            Connection conn = this.getConnection();
             try {
-                //TODO execute all queries in internal stack
-                //TODO commit
+                /* Disable autocommit */
+                conn.setAutoCommit(false);
+
+                Statement stm = conn.createStatement();
+                /* Execute every query in stack */
+                for (Iterator<String> iterator = this.queries.iterator(); iterator.hasNext();) {
+                    stm.addBatch(iterator.next());
+                }
+
+                /* Execute batch and commit */
+                stm.executeBatch();
+                conn.commit();
+                stm.close();
             } finally {
-                conn.close(); // close the connection
+                /* Enable autocommit */
+                conn.setAutoCommit(true);
+                /* Close connection */
+                conn.close();
             }
         } catch (SQLException ex) {
             Logger.getLogger(Connector.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        //TODO clear internal stack
+        this.queries.clear();
+    }
+
+
+    public ResultSet executeQueryWithResults(String query) {
+        /* Execute query in parameter and return ResultSet */
+        ResultSet result = null;
+        try {
+            /* Connect to the database */
+            Connection conn = this.getConnection();
+            try {
+                Statement stm = conn.createStatement();
+
+                /* Execute query */
+                result = stm.executeQuery(query);
+
+                /* Close statement */
+                stm.close();
+            } finally {
+                /* Close connection */
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Connector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        /* Return result */
+        return result;
     }
 }
