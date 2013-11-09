ba0cbd55ba91d465cba653b371ce6bbb512c016d
Jan Jeřábek <xjerab13@stud.fit.vutbr.cz>
2013-11-09 20:39:00 +0100

    Minor class update.

--- src/cz/vutbr/fit/pdb/control/MapControl.java
+++ src/cz/vutbr/fit/pdb/control/MapControl.java
@@ -4,10 +4,58 @@
  */
 package cz.vutbr.fit.pdb.control;
 
+import cz.vutbr.fit.pdb.containers.SpatialContainer;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+
 /**
  *
  * @author casey
  */
-public class MapControl {
+public class MapControl implements MouseInputListener{
+    
+    private JPanel view;
+    private SpatialContainer model;
+    
+    public MapControl(JPanel view, SpatialContainer model){
+        this.view = view;
+        this.model = model;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
