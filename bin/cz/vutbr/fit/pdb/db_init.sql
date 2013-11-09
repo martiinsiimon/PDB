9eabf094f19dbec3c06fec3c93c660748baf9ae9
Milan Barta <milanbarta@gmail.com>
2013-11-10 00:04:06 +0100

    Old db_init.sql deleted

--- bin/cz/vutbr/fit/pdb/db_init.sql
+++ bin/cz/vutbr/fit/pdb/db_init.sql
@@ -11,6 +11,8 @@ DROP TABLE beds CASCADE CONSTRAINTS;
 DROP TABLE plants CASCADE CONSTRAINTS;
 DROP TABLE layers CASCADE CONSTRAINTS;
 
+DELETE FROM user_sdo_geom_metadata;
+
 --------------------------------------------------------------------------------
 -- LAYERS --
 --------------------------------------------------------------------------------
@@ -96,7 +98,44 @@ CREATE TABLE soil (
 DROP SEQUENCE id_soil_seq;
 CREATE SEQUENCE id_soil_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE;
 
+INSERT INTO user_sdo_geom_metadata values (
+  'soil', 'geometry',
+  sdo_dim_array(sdo_dim_element('X', 0, 1000, 1),
+  sdo_dim_element('Y', 0, 700, 1)),
+  NULL);
+
 -- Data: --
+
+INSERT INTO soil VALUES (
+    id_soil_seq.nextval,
+    (select id from layers where name = 'soil'),
+    (select id from soil_type where name = 'black soil'),
+    SDO_GEOMETRY(2003, NULL, NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1003, 3),
+        SDO_ORDINATE_ARRAY(0,300, 820,700))
+);
+
+INSERT INTO soil VALUES (
+    id_soil_seq.nextval,
+    (select id from layers where name = 'soil'),
+    (select id from soil_type where name = 'brown soil'),
+    SDO_GEOMETRY(2003, NULL, NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1003, 3),
+        SDO_ORDINATE_ARRAY(0,0, 1000,300))
+);
+
+INSERT INTO soil VALUES (
+    id_soil_seq.nextval,
+    (select id from layers where name = 'soil'),
+    (select id from soil_type where name = 'sand'),
+    SDO_GEOMETRY(2003, NULL, NULL,
+        SDO_ELEM_INFO_ARRAY(1, 1003, 3),
+        SDO_ORDINATE_ARRAY(820,300, 1000,700))
+);
+
+-- Validation: --
+
+SELECT s.id, s.geometry.ST_IsValid() FROM soil s;
 
 --------------------------------------------------------------------------------
 -- PATHS --
@@ -161,7 +200,7 @@ CREATE SEQUENCE id_fences_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE;
 CREATE TABLE plants(
 
     id INT PRIMARY KEY,
-    name VARCHAR2(16),
+    name VARCHAR2(64),
     photo ORDSYS.ORDIMAGE,
     photo_sig ORDSYS.ORDIMAGESIGNATURE
 
@@ -171,6 +210,10 @@ DROP SEQUENCE id_plants_seq;
 CREATE SEQUENCE id_plants_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE;
 
 -- Data: --
+
+INSERT INTO plants VALUES (
+    id_plants_seq.nextval, 'genericka kvetina', NULL, NULL
+);
 
 --------------------------------------------------------------------------------
 -- SIGNS --
@@ -194,6 +237,46 @@ DROP SEQUENCE id_signs_seq;
 CREATE SEQUENCE id_signs_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE;
 
 -- Data: --
+
+INSERT INTO signs VALUES (
+    id_signs_seq.nextval,
+    (select id from layers where name = 'signs'),
+    SDO_GEOMETRY(2001, NULL, NULL,
+        SDO_ELEM_INFO_ARRAY(1 ,1 ,1),
+        SDO_ORDINATE_ARRAY(158, 517)),
+    'Popis genericke kvetiny',
+    (select id from plants where name = 'genericka kvetina'),
+    TO_DATE('08-04-2011', 'MM-DD-YYYY'),
+    TO_DATE('12-31-9999', 'MM-DD-YYYY')
+);
+
+INSERT INTO signs VALUES (
+    id_signs_seq.nextval,
+    (select id from layers where name = 'signs'),
+    SDO_GEOMETRY(2001, NULL, NULL,
+        SDO_ELEM_INFO_ARRAY(1 ,1 ,1),
+        SDO_ORDINATE_ARRAY(197, 482)),
+    'Popis genericke kvetiny',
+    (select id from plants where name = 'genericka kvetina'),
+    TO_DATE('08-04-2011', 'MM-DD-YYYY'),
+    TO_DATE('12-31-9999', 'MM-DD-YYYY')
+);
+
+INSERT INTO signs VALUES (
+    id_signs_seq.nextval,
+    (select id from layers where name = 'signs'),
+    SDO_GEOMETRY(2001, NULL, NULL,
+        SDO_ELEM_INFO_ARRAY(1 ,1 ,1),
+        SDO_ORDINATE_ARRAY(302, 517)),
+    'Popis genericke kvetiny',
+    (select id from plants where name = 'genericka kvetina'),
+    TO_DATE('08-04-2011', 'MM-DD-YYYY'),
+    TO_DATE('12-31-9999', 'MM-DD-YYYY')
+);
+
+-- Validation: --
+
+SELECT s.id, s.geometry.ST_IsValid() FROM signs s;
 
 --------------------------------------------------------------------------------
 -- BED_TYPE --
