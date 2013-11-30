3cab7db4bfb70db98c385199908dfbfcdd01bbfa
Jan Jeřábek <xjerab13@stud.fit.vutbr.cz>
2013-11-30 22:47:08 +0100

    class add

---
+++ src/cz/vutbr/fit/pdb/control/RootControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.vutbr.fit.pdb.control;
+
+import cz.vutbr.fit.pdb.containers.SpatialContainer;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+
+/**
+ *
+ * @author casey
+ */
+public class RootControl{
+    
+    private JPanel view;
+    
+    
+    public RootControl(JPanel view){
+        this.view = view;
+        
+    }
+
+    
+}
