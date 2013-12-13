de3005c38fc765965f2798a2b96a4185041d39de
Martin Simon <martiin.siimon@gmail.com>
2013-12-13 22:37:07 +0100

    Object for DB tables - draft

---
+++ src/cz/vutbr/fit/pdb/model/WaterObject.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.vutbr.fit.pdb.model;
+
+/**
+ * Java object for water from DB
+ *
+ * @author martin
+ */
+public class WaterObject {
+
+}
