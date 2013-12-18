5826554b4d7c75d43f8894ca14a37a751d70a09f
Martin Simon <martiin.siimon@gmail.com>
2013-12-18 20:32:57 +0100

    Fixed update after click

--- src/cz/vutbr/fit/pdb/control/MapControl.java
+++ src/cz/vutbr/fit/pdb/control/MapControl.java
@@ -43,6 +43,9 @@ public class MapControl{
             }
             so.setSelection(true);
             model.setSelected(so);
+
+            model.checkHovering(e.getPoint(), view.getAffineTransform());
+            view.updateUI();
         }
 
         @Override
