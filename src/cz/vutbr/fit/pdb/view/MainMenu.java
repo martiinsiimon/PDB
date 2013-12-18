22416896580c26115551b8cdf9bb3e3697ac38c4
Milan Barta <milanbarta@gmail.com>
2013-12-19 00:50:58 +0100

    Multimedial operation dialog added to menu
    Signed-off-by: Milan Barta <milanbarta@gmail.com>

--- src/cz/vutbr/fit/pdb/view/MainMenu.java
+++ src/cz/vutbr/fit/pdb/view/MainMenu.java
@@ -21,6 +21,7 @@ public class MainMenu extends JMenuBar{
     private JMenu mFile, mAbout, mEdit, mDatabase, mSpatial, mMultimedial, mTemporal;
     private JMenuItem mF_quit, mD_Connect, mD_Init, mA_Help, mA_About;
     private JMenuItem mS_BedBySoil, mS_BedsWithFences, mS_DistBtwBeds, mS_BiggestBed, mS_SmallestBed;
+    private JMenuItem mM_FindSimilar;
     private JCheckBoxMenuItem mE_cbEdit;
     
     
@@ -56,6 +57,9 @@ public class MainMenu extends JMenuBar{
         mS_SmallestBed = new JMenuItem("Show smallest beds");
         mS_SmallestBed.setActionCommand("s_smallest_bed");
         
+        mM_FindSimilar = new JMenuItem("Find the most similar by picture");
+        mM_FindSimilar.setActionCommand("m_find_similar");
+        
         mA_Help = new JMenuItem("Help");
         mA_Help.setActionCommand("a_help");
         
@@ -74,6 +78,8 @@ public class MainMenu extends JMenuBar{
         mSpatial.add(mS_DistBtwBeds);
         mSpatial.add(mS_BiggestBed);
         mSpatial.add(mS_SmallestBed);
+        
+        mMultimedial.add(mM_FindSimilar);
         
         mAbout.add(mA_Help);
         mAbout.add(mA_About);
@@ -104,6 +110,7 @@ public class MainMenu extends JMenuBar{
         this.mS_DistBtwBeds.addActionListener(controler);
         this.mS_BiggestBed.addActionListener(controler);
         this.mS_SmallestBed.addActionListener(controler);
+        this.mM_FindSimilar.addActionListener(controler);
         this.mA_About.addActionListener(controler);
         this.mA_Help.addActionListener(controler);
     }
